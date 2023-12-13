ee540da218cdf612237bc7ae31efd8c98a7577a5
farahbakhsh3 <farahbakhsh3@gmail.com>
2023-12-13 22:52:10 +0330

    solver complted. random set_1to10 implemented. main method created. all features done. 😉😉😉

--- src/sudokusolver/Sudoku.java
+++ src/sudokusolver/Sudoku.java
@@ -1,36 +1,45 @@
 package sudokusolver;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Sudoku {
 
-    private int[][] board;
+    private final int[][] board;
+    private Integer[] set_1to10 = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    private List<Integer> list_1to10 = Arrays.asList(set_1to10);
 
     public Sudoku(int[][] board) {
         this.board = board;
+
+        Collections.shuffle(this.list_1to10);
+        this.list_1to10.toArray(this.set_1to10);
     }
 
     @Override
     public String toString() {
         String output;
         output = "|-----------------------|\n";
-        for (int row = 0; row <= 9; row++) {
+        for (int row = 0; row < 9; row++) {
             output += "| ";
-            for (int col = 0; col <= 9; col++) {
+            for (int col = 0; col < 9; col++) {
                 output += this.board[row][col] + " ";
                 if (col % 3 == 2) {
                     output += "| ";
                 }
-                output += "\n";
-                if (row % 3 == 2) {
-                    output += "|-----------------------|\n";
-                }
+            }
+            output += "\n";
+            if (row % 3 == 2) {
+                output += "|-----------------------|\n";
             }
         }
         return output;
     }
 
     private int[] findFirstFreePlace() {
-        for (int row = 0; row <= 9; row++) {
-            for (int col = 0; col <= 9; col++) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
                 if (this.board[row][col] == 0) {
                     return new int[]{row, col};
                 }
@@ -40,40 +49,48 @@ public class Sudoku {
     }
 
     private Boolean isValidMove(int row, int col, int num) {
-    for (int c=0; c <= 9; c++)
-            if (this.board[row][c] == num)
+        for (int c = 0; c < 9; c++) {
+            if (this.board[row][c] == num) {
                 return false;
-        
-    for (int r=0; r <= 9; r++)
-            if (this.board[r][col] == num)
-                return false;
+            }
+        }
 
-    int top = row / 3 * 3;
-    int left = col / 3 * 3;
-    for (int r=0; r <= 3; r++)
-        for (int c=0; c <= 3; c++)
-            if (this.board[top + r][left + c] == num)
+        for (int r = 0; r < 9; r++) {
+            if (this.board[r][col] == num) {
+                return false;
+            }
+        }
+
+        int top = row / 3 * 3;
+        int left = col / 3 * 3;
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (this.board[top + r][left + c] == num) {
                     return false;
-    
+                }
+            }
+        }
         return true;
     }
-    
-    public Boolean solveBoard(){
+
+    public Boolean solveBoard() {
         int[] position = this.findFirstFreePlace();
         int row = position[0];
         int col = position[1];
-        
-        if ((row == -1) || (col == -1))
-            return true;
 
-        for (int num=1; num <=9; num++){
-            if (this.isValidMove(num, row, col)){
-                this.board[row][col] = num;
-                if (this.solveBoard())
+        if ((row == -1) || (col == -1)) {
+            return true;
+        }
+
+        for (int num = 0; num < 9; num++) {
+            if (this.isValidMove(row, col, this.set_1to10[num])) {
+                this.board[row][col] = this.set_1to10[num];
+                if (this.solveBoard()) {
                     return true;
-                this.board[row][col] = 0;        
+                }
+                this.board[row][col] = 0;
             }
         }
         return false;
-    }    
+    }
 }
--- src/sudokusolver/SudokuSolver.java
+++ src/sudokusolver/SudokuSolver.java
@@ -4,7 +4,24 @@ package sudokusolver;
 public class SudokuSolver {
 
         public static void main(String[] args) {
-
+            
+            int[][] board = new int[][] {
+                {9,0,0,0,0,0,0,0,0},
+                {0,0,0,0,9,0,0,0,0},
+                {0,0,0,0,0,0,9,0,0},
+                {0,0,0,9,0,0,0,0,0},
+                {0,9,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,9,0},
+                {0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0},
+            };
+            Sudoku sudoku = new Sudoku(board);
+            System.out.println(sudoku.toString());
+            if(sudoku.solveBoard())
+                System.out.println(sudoku.toString());
+            else
+                System.err.println("There is no sulotion for this board!!!");
     }
     
 }
